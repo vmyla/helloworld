46aef6b34442c39ea8beeca9fa2c2b3188467d62
venkateswara.km <venkateswara.km@flipkart.com>
2016-01-27 21:55:15 +0530

    adding hello world class

---
+++ hellomodule/src/main/java/com/test/util/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.test.util;
+
+/**
+ * Created by venkateswara.km on 27/01/16.
+ */
+public class HelloWorld {
+
+  public static void main(String[] args){
+    System.out.println("Welcome to Git World");
+  }
+}
