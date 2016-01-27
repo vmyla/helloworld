656f00618874c0e7ad841613c2b5c5c232623c3e
venkateswara.km <venkateswara.km@flipkart.com>
2016-01-27 23:33:26 +0530

    adding central maven repository for dependency management

--- hellomodule/src/main/java/com/test/util/HelloWorld.java
+++ hellomodule/src/main/java/com/test/util/HelloWorld.java
@@ -1,11 +1,18 @@
 package com.test.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.logging.LogManager;
+
 /**
  * Created by venkateswara.km on 27/01/16.
  */
 public class HelloWorld {
 
+  private static final Logger logger = LoggerFactory.getLogger(HelloWorld.class);
   public static void main(String[] args){
     System.out.println("Welcome to Git World");
+    logger.error("Hello World Error");
   }
 }
